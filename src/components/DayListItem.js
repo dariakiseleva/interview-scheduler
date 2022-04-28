de4154cbd7ce554ac550d36758e40c37aed0d8fe
dariakiseleva <daria.kiseleva@mail.mcgill.ca>
2022-04-28 10:54:51 -0400

    Add tests for the DayListItem component and format the display of remaining spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,11 +4,22 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  //Create a message about remaining spots depending on the number
+  const formatSpots = (numSpots) => {
+    if (numSpots===0) {
+      return "no spots remaining"
+    }
+    if (numSpots===1) {
+      return "1 spot remaining"
+    }
+    return `${numSpots} spots remaining`
+  }
+
   let dayListItemClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots===0})
   return (
     <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
