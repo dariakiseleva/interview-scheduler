d9ad2c9168492362ea1d3cd7c54915f4a58c7106
dariakiseleva <daria.kiseleva@mail.mcgill.ca>
2022-04-27 22:44:44 -0400

    Build button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,23 @@
 import React from "react";
+import classNames from "classnames";
+
 
 import "components/Button.scss";
 
 export default function Button(props) {
 
-   let buttonClass = "button";
-
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-    }
-
+   //Build a string for class names depending on true/false values of props
+   let buttonClass = classNames('button', {"button--confirm": props.confirm, "button--danger": props.danger});
 
    return (
 
-   <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
+   <button 
+      className={buttonClass}
+      onClick={props.onClick} 
+      disabled={props.disabled}>
+
    {props.children}
+
    </button>
 
    );
