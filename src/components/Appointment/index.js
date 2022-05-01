7a99180e26e295ac8fc38b8a113c430600787d81
dariakiseleva <daria.kiseleva@mail.mcgill.ca>
2022-05-01 19:15:06 -0400

    Use hook to transition between Empty, Show, and Create modes of the Appointment components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,50 @@
 import React, { Fragment } from 'react'
 import "./styles.scss";
+
+//Import functionalities
+import useVisualMode from "../hooks/useVisualMode";
+
+//Import components
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import Form from "./Form"
 
+//Mode constants
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+
+//Render appointment component
 export default function Appointment(props){
+
+  //Destructure what the hook returns
+  const { mode, transition, back } = useVisualMode(
+    //Initial mode is SHOW or EMPTY depending on if interview exists
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <Fragment>
       <Header time={props.time} />
-      {props.interview ? 
-        <Show 
-          interviewer={props.interview.interviewer} 
+
+      {/* //EMPTY or SHOW components are initially rendered depending on the initial mode */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
-        /> 
-        : <Empty/>
-      }
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+        />
+      )}
+
+
     </Fragment>
   );
 
